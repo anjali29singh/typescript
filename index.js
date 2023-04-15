395f973c5b6cc5692f196fecf017ab957e10589d
anjali29singh <anjalisingh930260@gmail.com>
2023-04-15 23:52:16 +0530

    2nd-commit

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ var user = {
     id: 23455,
 };
 console.log(typeof user);
+// function add(a, b) {
+//   console.log(a + b);
+// }
+// add(3, "3");
+var add = function (a, b) {
+    return a + b;
+};
+var sum = add(2, 5);
+console.log(sum);
+var fail = true;
+console.log(typeof fail);
+var userId = 1234556;
+console.log(typeof userId);
+// const  marks:arrayType =[1,2,'a'] //error
+var marks = [1, 2, 4, 5, 6, 7, 9];
+console.log(typeof marks);
--- index.ts
+++ index.ts
@@ -11,3 +11,33 @@ const user: User = {
   id: 23455,
 };
 console.log(typeof user);
+
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// add(3, "3");
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+const sum = add(2, 5);
+console.log(sum);
+
+type boolType = true | false;
+const fail: boolType = true;
+console.log(typeof fail);
+
+type id = number | string;
+const userId: id = 1234556;
+console.log(typeof userId);
+
+//Generics : this provides variable to types
+
+//In  js array can contain elements with different data types
+
+type arrayType = Array<number>;
+
+// const  marks:arrayType =[1,2,'a'] //error
+
+const marks: arrayType = [1, 2, 4, 5, 6, 7, 9];
