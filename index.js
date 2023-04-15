ff2c1970868959038001f236c1bf6cd65c0f1de6
anjali29singh <anjalisingh930260@gmail.com>
2023-04-15 22:30:41 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var message = "hello World";
+console.log(typeof message);
+var user = {
+    username: "anjali",
+    id: 23455,
+};
+console.log(typeof user);
---
+++ index.ts
@@ -0,0 +1,13 @@
+var message: string = "hello World";
+console.log(typeof message);
+
+interface User {
+  username: String;
+  id: Number;
+}
+
+const user: User = {
+  username: "anjali",
+  id: 23455,
+};
+console.log(typeof user);
